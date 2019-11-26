ed7c81508e7bee630eca9ca1e79520ad109a75b6
Will <master_wu51@hotmail.com>
2019-11-26 21:00:46 +1000

    change url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://dry-brook-23811.herokuapp.com/"
+  uri: "https://tranquil-fortress-45340.herokuapp.com/"
 });
 
 const authLink = setContext(() => {
